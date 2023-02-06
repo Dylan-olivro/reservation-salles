bd2593279372ce67517a2f338a83587169bebab0
dylan-olivro <dylan.olivro@laplateforme.io>
2023-02-06 11:40:11 +0100

    ajout page planning.php

--- php/include/header-index.php
+++ php/include/header-index.php
@@ -2,6 +2,7 @@
 if (isset($_SESSION['login'])) {
     echo '<a href="./index.php">Accueil</a>';
     echo '<a href="./php/profil.php">Profil</a>';
+    echo '<a href="./php/planning.php">Planning</a>';
     if ($_SESSION["login"] == "admin") {
         echo '<a href="./php/admin.php">Admin</a>';
     }
