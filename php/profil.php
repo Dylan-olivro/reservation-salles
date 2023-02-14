1ee48c9d213099424c03b5a419250f06da36a8bf
dylan-olivro <dylan.olivro@laplateforme.io>
2023-02-14 09:43:01 +0100

    ajout du css sur differente pages

--- php/profil.php
+++ php/profil.php
@@ -11,7 +11,7 @@ require "./include/config.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="../css/common.css">
-    <link rel="stylesheet" href="../css/signup.css">
+    <link rel="stylesheet" href="../css/inscription.css">
     <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
