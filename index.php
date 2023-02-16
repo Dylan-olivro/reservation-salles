c83edc309676c4ac8489eb14c766f6403946b443
CharlesCaltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-02-16 11:25:06 +0100

    un peu de responsive sur le planning.css link index.css mis en commentaire dans le index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require "./php/include/config.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="./css/common.css">
-    <link rel="stylesheet" href="./css/index.css">
+    <!-- <link rel="stylesheet" href="./css/index.css"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
